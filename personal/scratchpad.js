dd3fa4a36b728c96cb60b13aec96d464ee7e53a4
JPK583 <joseph.p.kelly@sherwin.com>
2025-09-18 17:27:28 +0000

    scratchpad stuff lol

--- personal/scratchpad.js
+++ personal/scratchpad.js
@@ -1,1 +1,33 @@
+function make_greeter(season="Day") {
+    //let season = "Halloween"
+    return function() {
+        console.log(`Happy ${season}!`)
+    }
+}
 
+greeter = make_greeter("Leaf Erikson Day")
+
+greeter()
+
+
+class MathTools {
+    static double(x) {
+        return x+x
+    }
+    square(x) {
+        return x*x
+    }
+}
+
+mt = MathTools()
+mt.square(4)
+
+MathTools.double(4)
+
+const x = 10;
+const obj = { x };
+console.log(obj);
+
+const math = {
+    add: function(a,b) { return a+b }
+}
